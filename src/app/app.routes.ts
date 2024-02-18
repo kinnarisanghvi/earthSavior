ecc0151cf79148659ee91c75c53b3fb078ad0cf2
kinnari sanghvi <sanghvi.ki@northeastern.edu>
2024-02-18 10:51:35 -0500

    More changes for camera

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import {WebcamComponent} from "ngx-webcam";
+import {HeaderComponent} from "./components/header/header.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'header', component: HeaderComponent },
+];
