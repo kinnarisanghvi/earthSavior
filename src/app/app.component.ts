cdb4be81154170d6e149811e64ec1b3b2f5fc71b
kinnari sanghvi <sanghvi.ki@northeastern.edu>
2024-02-17 20:59:35 -0500

    More changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,40 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {Observable, Subject} from "rxjs";
+import {WebcamImage, WebcamModule} from "ngx-webcam";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, WebcamModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'earthSaviors';
+
+  private trigger: Subject<any> = new Subject();
+  webcamImage: any;
+  private nextWebcam: Subject<any> = new Subject();
+
+  sysImage = '';
+
+  ngOnInit() {}
+
+  public getSnapshot(): void {
+    this.trigger.next(void 0);
+  }
+
+  public captureImg(webcamImage: WebcamImage): void {
+    this.webcamImage = webcamImage;
+    this.sysImage = webcamImage!.imageAsDataUrl;
+    console.info('got webcam image', this.sysImage);
+  }
+
+  public get invokeObservable(): Observable<any> {
+    return this.trigger.asObservable();
+  }
+
+  public get nextWebcamObservable(): Observable<any> {
+    return this.nextWebcam.asObservable();
+  }
 }
